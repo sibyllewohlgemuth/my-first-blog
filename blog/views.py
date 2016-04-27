b8492c65d67bb34aa629aebf4063e48230f58c6d
sibyllewohlgemuth <sibyllewohlgemuth@hotmail.com>
2016-04-27 15:01:29 +0200

    authorized add/edit of post, added user login/logout.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
+from django.contrib.auth.decorators import login_required
+
 from django.utils import timezone
 
 from .models import Post
@@ -12,6 +14,17 @@ def post_list(request):
 
 	return render(request, 'blog/post_list.html', {'posts':posts});
 
+	
+
+@login_required
+def post_draft_list(request):
+
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+	return render(request, 'blog/post_draft_list.html', {'posts':posts});
+
+	
+
 
 def post_detail(request, pk):
 
@@ -20,6 +33,7 @@ def post_detail(request, pk):
 	return render(request, 'blog/post_detail.html', {'post': post});
 
 
+@login_required
 def post_new(request):
 
 	if request.method == "POST":
@@ -31,8 +45,6 @@ def post_new(request):
 			post = form.save(commit=False)
 
 			post.author = request.user
-
-			post.published_date = timezone.now()
 
 			post.save()
 
@@ -47,7 +59,7 @@ def post_new(request):
 
 
 
-
+@login_required
 def post_edit(request, pk):
 
 	post = get_object_or_404(Post, pk=pk)
@@ -73,5 +85,27 @@ def post_edit(request, pk):
 		form = PostForm(instance=post)
 
 	return render(request, 'blog/post_edit.html', {'form': form});
+
+
+@login_required
+def post_publish(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+
+    post.publish()
+
+    return redirect('blog.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+
+    post.delete()
+    
+    return redirect('blog.views.post_list')
+
+
+
 
 
